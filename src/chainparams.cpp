e4e5893183994382c1490356d158ee3dfc9f200e
root <root@vmi339182.contaboserver.net>
2020-01-31 20:48:36 +0100

    test Hashshare

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -381,7 +381,7 @@ public:
 		fRequireRoutableExternalIP = true;
 		fMineBlocksOnDemand = false;
 		fAllowMultipleAddressesFromGroup = false;
-		fAllowMultiplePorts = false;
+		fAllowMultiplePorts = true;
 
 		nPoolMinParticipants = 3;
 		nPoolMaxParticipants = 5;
